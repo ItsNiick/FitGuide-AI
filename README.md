729f3c42386a51a57fbc5647d76a61f7714e2ba2
Nicholas Shedd <nshedd99@gmail.com>
2026-05-15 15:16:00 -0700

    minor fixes and README update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # FitGuide-AI
-Small AI powered Python project that helps users create a custom workout routine.
+
+## Files: 
+- streamlit_app.py: Main Streamlit web application and user interface.
+- workout_ai.py: Handles Gemini API connection and workout generation.
+- requirements.txt: Lists required Python packages (Mainly used for Streamlit Deployment)
+- images/: Stores icon for web application.
+
+## Setup:
+1. Install dependencies with pip install -r requirements.txt
+2. Add your own GEMINI_API_KEY to a local .env file
+3. Run the app with python -m streamlit run streamlit_app.py
--- workout_ai.py
+++ workout_ai.py
@@ -4,20 +4,25 @@ from dotenv import load_dotenv
 from google import genai
 import streamlit as st
 
+# Loads variables from .env file
 load_dotenv()
 
+# Function to get API key from Streamlit secrets or .env file
 def get_api_key():
     try:
         return st.secrets["GOOGLE_API_KEY"]
     except Exception:
         return os.getenv("GOOGLE_API_KEY")
-    
+
+# Stores the API key from secrets or .env file
 api_key = get_api_key()
 
+# If API key is not correctly found, raise an error alerting that it needs to be set up
 if not api_key:
     raise ValueError("GOOGLE_API_KEY is not set. Add it to Streamlit Secrets or your .env file.")
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+# Creates the GenAI client using the API key
+client = genai.Client(api_key=api_key)
 
 def generate_workout(user_profile):
     prompt = f"""
@@ -70,13 +75,14 @@ Rules:
 - Do not include text outside the JSON
 - Always include a disclaimer at the end that I am not a doctor but instead an extension of Google Gemini designed by Nicholas Shedd, and that not all information may be correct for you and your fitness goals.
 """
-
+    # Sends the prompt to Gemini 2.5 Flash Lite and asks the model to generate a response
     response = client.models.generate_content(
         model="gemini-2.5-flash-lite",
         contents=prompt
     )
 
     try:
+        # Gets the text response from the model and parses it as JSON into a Python dictionary that Streamlit uses
         return json.loads(response.text)
     except json.JSONDecodeError:
         return {
